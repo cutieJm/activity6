b97702552fc52fdfb7f6bd83f1e268d2a3630632
cutieJm <143053232+cutieJm@users.noreply.github.com>
2023-11-23 09:28:56 +0800

    initial change

---
+++ src/User.jsx
@@ -0,0 +1,123 @@
+export const User = [
+    {
+      id: 1,
+      name: {
+        firstname: "MARLON",
+        lastname: "CARBONILLA",
+      },
+      course: "BSIT",
+      yearsection: "3A",
+      username: "LONZKIE",
+      password: "1212",
+      usertype: "STUDENT",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "JOHN CARLO",
+        lastname: "NAMOR",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "LOLOY",
+      password: "1465",
+      usertype: "STUDENT",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "JESSA",
+        lastname: "RESMA",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "JAVA QUEEN",
+      password: "7878",
+      usertype: "STUDENT",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "JOHN MARK",
+        lastname: "LAGRIMAS",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "JOHN",
+      password: "7777",
+      usertype: "STUDENT",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "JOVERT",
+        lastname: "DAVIRAO",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "BATMAN",
+      password: "5555",
+      usertype: "STUDENT",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "JAMES",
+        lastname: "NOBLE",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "JAMIS",
+      password: "2322",
+      usertype: "STUDENT",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "JERRY",
+        lastname: "ALE",
+      },
+      course: "",
+      yearsection: "",
+      username: "JERRY",
+      password: "8722",
+      usertype: "TEACHER",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "GABRENE",
+        lastname: "DIAZ",
+      },
+      course: "",
+      yearsection: "",
+      username: "BENJ",
+      password: "7888",
+      usertype: "TEACHER",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "FERMO",
+        lastname: "ASUFRA",
+      },
+      course: "",
+      yearsection: "",
+      username: "FERMO",
+      password: "5699",
+      usertype: "TEACHER",
+    },
+    {
+      id: 10,
+      name: {
+        firstname: "CHRISTIAN",
+        lastname: "OMOSURA",
+      },
+      course: "BSCS 2",
+      yearsection: "",
+      username: "TIAN",
+      password: "3330",
+      usertype: "STUDENT",
+    },
+    
+  ];
